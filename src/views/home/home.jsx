aff673b519656adf25b87f0b44ae1608285a89c0
ishanajmeri <ajmeriishansurat@gmail.com>
2020-07-01 20:54:39 +0530

    final

--- src/views/home/home.jsx
+++ src/views/home/home.jsx
@@ -3,11 +3,14 @@ import { Descriptions, Card } from 'antd';
 class Home extends Component {
   state = {};
   render() {
+    const { user } = this.props;
     return (
       <Card style={{ padding: '2%' }}>
         <Descriptions title="User Info">
-          <Descriptions.Item label="UserName">Ishan ajmeri</Descriptions.Item>
-          <Descriptions.Item label="Telephone">922xxxxx55</Descriptions.Item>
+          <Descriptions.Item label="UserName">
+            {user === null ? user.username : '-'}
+          </Descriptions.Item>
+          <Descriptions.Item label="Telephone">9xxxxxxx55</Descriptions.Item>
           <Descriptions.Item label="Live">Surat,Gujarat</Descriptions.Item>
           <Descriptions.Item label="Remark">empty</Descriptions.Item>
           <Descriptions.Item label="Address">
